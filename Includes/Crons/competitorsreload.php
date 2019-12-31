7c2e15b546f4d411f6dbb8b25eadfb18896d283d
SUPHAIR <suphair@gmail.com>
2019-12-31 22:57:18 +0300

    Update competitorsreload.php

--- Includes/Crons/competitorsreload.php
+++ Includes/Crons/competitorsreload.php
@@ -1,22 +1,25 @@
 <?php
 $start=date("H:i:s");
+$limitApi=10;
+$limitBd=1000;
+$depth=14;
 
-DataBaseClass::Query(" Select UpdateTimestamp, ID, WID from Competitor  where  WID is not null and WCAID='' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > 7
-order by UpdateTimestamp Limit 10");
+DataBaseClass::Query(" Select UpdateTimestamp, ID, WID from Competitor  where  WID is not null and WCAID='' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > $depth
+order by UpdateTimestamp Limit $limitApi");
 
 foreach(DataBaseClass::getRows() as $user){
     Competitors_Reload($user['ID'],$user['WID']);
 }
 
-DataBaseClass::Query(" Select UpdateTimestamp, ID, WCAID from Competitor  where  WID is null and WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > 7
-order by UpdateTimestamp Limit 10");
+DataBaseClass::Query(" Select UpdateTimestamp, ID, WCAID from Competitor  where  WID is null and WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > $depth
+order by UpdateTimestamp Limit $limitApi");
 
 foreach(DataBaseClass::getRows() as $user){
     Competitors_Reload($user['ID'],$user['WCAID']);
 }
  
-DataBaseClass::Query("select ID, WCAID, Country, Name from Competitor where WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > 7 
-order by UpdateTimestamp limit 50");
+DataBaseClass::Query("select ID, WCAID, Country, Name from Competitor where WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > $depth 
+order by UpdateTimestamp limit $limitBd");
 
 foreach(DataBaseClass::getRows() as $row){
     DataBaseClassWCA::Query("select P.id,C.iso2, trim(SUBSTRING_INDEX(P.name,'(',1)) name from Persons P join Countries C on C.id=P.countryId where P.id='{$row['WCAID']}'");    
@@ -40,7 +43,7 @@ Competitors_RemoveDuplicates();
 $end=date("H:i:s");
 
 
-DataBaseClass::Query(" Select UpdateTimestamp, ID, WID from Competitor  where  WID is not null and WCAID='' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > 7 ");
+DataBaseClass::Query(" Select UpdateTimestamp, ID, WID from Competitor  where  WID is not null and WCAID='' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > $depth ");
 $count1=DataBaseClass::rowsCount();
 
 DataBaseClass::Query(" Select UpdateTimestamp, ID, WID from Competitor  where  WID is not null and WCAID='' ");
@@ -48,14 +51,14 @@ $count1t=DataBaseClass::rowsCount();
 
 
 
-DataBaseClass::Query(" Select UpdateTimestamp, ID, WCAID from Competitor  where  WID is null and WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > 7 ");
+DataBaseClass::Query(" Select UpdateTimestamp, ID, WCAID from Competitor  where  WID is null and WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > $depth");
 $count2=DataBaseClass::rowsCount();
 
 DataBaseClass::Query(" Select UpdateTimestamp, ID, WCAID from Competitor  where  WID is null and WCAID<>'' ");
 $count2T=DataBaseClass::rowsCount();
 
 
-DataBaseClass::Query("select ID, WCAID from Competitor where WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > 7 ");
+DataBaseClass::Query("select ID, WCAID from Competitor where WCAID<>'' and TO_DAYS(now()) - TO_DAYS(UpdateTimestamp) > $depth ");
 $count3=DataBaseClass::rowsCount();
 
 AddLog('CompetitorsReload', 'Cron',"$count1/$count1t $count2/$count2T $count3 : $start - $end");
