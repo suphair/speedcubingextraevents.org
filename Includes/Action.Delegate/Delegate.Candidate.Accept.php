615c5932344dee479c3ee50cd92c23cac9d1ddc1
SUPHAIR <suphair@gmail.com>
2019-12-31 10:14:54 +0300

    Mail about a new delegate

--- Includes/Action.Delegate/Delegate.Candidate.Accept.php
+++ Includes/Action.Delegate/Delegate.Candidate.Accept.php
@@ -16,6 +16,10 @@ if($data['RequestCandidate_ID']){
     DataBaseClass::Query("Insert into Delegate(`Name`,`WCA_ID`,`Status`,`WID`) values"
             . " ('".$data['Competitor_Name']."','".$data['Competitor_WCAID']."','Trainee',".$data['Competitor_WID'].")");    
     
+    $WCAID=$data['Competitor_WCAID'];
+    $Name=$data['Competitor_Name'];
+    SendMail(getini('Seniors','email'), 'SEE: New delegate '.$Name,
+            "<pre>".$Name." <br>SEE <a href='https://". PageIndex()."Delegate/$WCAID'>$Name</a><br>WCA <a href='https://www.worldcubeassociation.org/persons/$WCAID'>$WCAID</a><br>");
 }
 
 header('Location: '.$_SERVER['HTTP_REFERER']);
