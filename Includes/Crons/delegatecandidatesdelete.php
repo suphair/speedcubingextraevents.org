f728879aaa94a25be9ffe145b38a961aff2ca038
SUPHAIR <suphair@gmail.com>
2020-01-02 17:32:24 +0300

    Update delegatecandidatesdelete.php

--- Includes/Crons/delegatecandidatesdelete.php
+++ Includes/Crons/delegatecandidatesdelete.php
@@ -3,9 +3,9 @@
 AddLog('DelegateCandidatesDelete', 'Cron','Begin');
 DataBaseClass::Query(" Select * from RequestCandidate where Status=-1 and (TO_DAYS(now()) - TO_DAYS(Datetime)) >= 263");
 foreach(DataBaseClass::getRows() as $row){
-    DataBaseClass::Query(" Delete from RequestCandidateVote where Competitor=".$row['Competitor'],true);
-    DataBaseClass::Query(" Delete from RequestCandidateField where RequestCandidate=".$row['ID'],true);
-    DataBaseClass::Query(" Delete from RequestCandidate where Competitor=".$row['Competitor'],true);
+    DataBaseClass::Query(" Delete from RequestCandidateVote where Competitor=".$row['Competitor']);
+    DataBaseClass::Query(" Delete from RequestCandidateField where RequestCandidate=".$row['ID']);
+    DataBaseClass::Query(" Delete from RequestCandidate where Competitor=".$row['Competitor']);
     
     AddLog('DelegateCandidatesDelete', 'Cron',$row['Competitor']." ".$row['Datetime']);
 }
