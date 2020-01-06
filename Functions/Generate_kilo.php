629ad465841d3cbdbaaedf8649fa2a53ed9389db
SUPHAIR <suphair@gmail.com>
2020-01-06 18:01:29 +0300

    kilo

--- Functions/Generate_kilo.php
+++ Functions/Generate_kilo.php
@@ -1,20 +1,15 @@
 <?php 
 function Generate_kilo(){
-    $moveR=array("R++","R--");
-    $moveD=array("D++","D--");
-    $moveU=array("U ","U'");
-    $str="";
-    for($j=1;$j<=3;$j++){
-        for($i=1;$i<=5;$i++){
-            $str.=$moveR[array_rand($moveR)]." ";  
-            $str.=$moveD[array_rand($moveD)]." ";
-        }
-        $str.=$moveU[array_rand($moveU)]." ";
-        if($j==1 || $j==2){
-            $str.=" & ";
-        }
-    }
-
+    
+    $filename="Script/kilo_out.txt";
+    $file=file($filename);
+    
+    $i=rand(0,sizeof($file));
+    $fp=fopen($filename,"w");
+    $str=$file[$i];
+    unset($file[$i]);
+    fputs($fp,implode("",$file));
+    fclose($fp);
     $str=trim($str);
     return $str;
 }
--- Scramble/kilo.php
+++ Scramble/kilo.php
@@ -62,53 +62,36 @@ $im = imageCreateFromPng("Scramble/Template/Kilominx.png");
   }
   
   $circles=array(
-          'U'=>array(
-                array('61','62','63','64','65'),
-                array('42','53','14','25','31'),
-                array('35','41','52','13','24'),
-          ),
-          
-          'R'=>array(
-                array('71','73','75','72','74'),
-                array('54','84','B5','44','C2'),
-                array('55','85','B1','45','C3'),
-                
-                array('94','14','33','A4','61'),
-                array('95','15','34','A5','62'),
-                array('91','11','35','A1','63'),
-                
-                array('52','82','B3','42','C5'),
-                array('53','83','B4','43','C1'),
-                array('51','81','B2','41','C4'),
-          ),
-          
-          'D'=>array(
-                array('C1','C3','C5','C2','C4'),
-                array('B2','84','A1','73','95'),
-                array('B1','83','A5','72','94'),
-              
-                array('81','A3','75','92','B4'),
-                array('82','A4','71','93','B5'),
-                array('85','A2','74','91','B3'),
-              
-                array('44','22','55','33','11'),
-                array('43','21','54','32','15'),
-                array('45','23','51','34','12'),
-          )
+          'U'=>[['61','62','63','64','65'],['42','53','14','25','31'],['35','41','52','13','24']],
+          'R'=>[['51','52','53','54','55'],['63','41','75','B5','15'],['62','45','74','B4','14']],
+          'F'=>[['11','12','13','14','15'],['63','51','B3','A3','25'],['64','52','B4','A4','21']],
+          'L'=>[['21','22','23','24','25'],['65','13','A3','93','32'],['64','12','A2','92','31']],
+          'BR'=>[['41','42','43','44','45'],['61','34','85','75','53'],['62','35','81','71','54']],
+          'BL'=>[['31','32','33','34','35'],['61','24','92','82','43'],['65','23','91','81','42']],
       );
               
+  foreach(['1'=>'8','2'=>'9','3'=>'A','4'=>'B','5'=>'7','6'=>'C'] as $s1=>$s2){
+      for($i=1;$i<=5;$i++){
+         $circles['flip'][]=[$s1.$i,$s2.$i]; 
+      }
+  }
+  
   
 foreach(explode(" ",$scramble) as $move){
     $move=trim($move);
-    if($move<>"" and in_array($move[0],array("U","R","D"))){
-        $direct=true;
-        if(isset($move[1])){
-            $direct=false; 
-            if($move[1]=="+"){
-               $direct=true; 
+    $direct=strpos($move,"'")===false;
+    $double=strpos($move,"2")!==false;
+    $move= str_replace(["'",'2'], "", $move);
+    if($move<>""){
+        if(isset($circles[$move])){
+            if($double){
+                $CoorColor=Rotate($CoorColor,$circles,$move,$direct);                  
             }
+            $CoorColor=Rotate($CoorColor,$circles,$move,$direct);              
+        }else{
+            echo $move;
+            exit();
         }
-        $CoorColor=Rotate($CoorColor,$circles,$move[0],$direct);              
     }
 }
    
@@ -118,7 +101,6 @@ foreach(explode(" ",$scramble) as $move){
         //imageellipse($im, $center['x']+$coor['x'], $center['y']+$coor['y'], 100, 100,$Colors['Red']);
     }
   }
- 
   return $im;
 }
 ?>
