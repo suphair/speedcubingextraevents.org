7751405646c98f669fe980c0bd15e0442ba2b2a8
SUPHAIR <suphair@gmail.com>
2020-02-03 18:13:02 +0300

    Competitor.Login added icons

--- Includes/Page.Competitor/Competitor.Login.php
+++ Includes/Page.Competitor/Competitor.Login.php
@@ -3,10 +3,9 @@ $Competitor=GetCompetitorData();
 $Delegate=CashDelegate(); 
 
 if($Competitor){ ?>
-    <?= svg_green(12) ?>
-    <?= Short_Name($Competitor->name) ?> <a href="<?= PageIndex() ?>Actions/Competitor.Logout"><font color="red"><?= ml('Competitor.SignOut')?></font></a>
-        <a href="<?= PageIndex() ?>Competitor/<?= $Competitor->local_id ?>"><img src='<?= PageIndex()?>Image/Icons/noavatar.png' width='10px'> <?= ml('Competitor.MyResults') ?></a>
-        <a href="<?= PageIndex() ?>Competitions/My"><img src='<?= PageIndex()?>Image/Icons/mycompetitions.png' width='10px'> <?= ml('Competitor.MyCompetitions') ?></a>   
+    <?= Short_Name($Competitor->name) ?> 
+        <a href="<?= PageIndex() ?>Competitor/<?= $Competitor->local_id ?>"><img src='<?= PageIndex()?>Image/Icons/noavatar.png' width='12px'> <?= ml('Competitor.MyResults') ?></a>
+        <a href="<?= PageIndex() ?>Competitions/My"><img src='<?= PageIndex()?>Image/Icons/mycompetitions.png' width='12px'> <?= ml('Competitor.MyCompetitions') ?></a>   
         <?php if($Delegate){ ?>
             <nobr>&#9642; <a href="<?= LinkDelegate($Delegate['Delegate_WCA_ID']); ?>">
                 <?= ml('Competitor.Login.Delegate') ?>
@@ -32,11 +31,13 @@ if($Competitor){ ?>
         <?php } ?> 
                 
 <?php }else{ ?>
-    <?= svg_red(12) ?>
     <?php  $_SESSION['ReferAuth']=$_SERVER['REQUEST_URI']; ?> 
-    <a href="<?= GetUrlWCA(); ?>"><?= ml('Competitor.SignIn')?></a>
+    <a href="<?= GetUrlWCA(); ?>"><img src='<?= PageIndex()?>Image/Icons/signin.png' width='12px'> <?= ml('Competitor.SignIn')?></a>
 <?php } ?>
 <form class='form_inline' method="POST" action="<?=PageAction('Language.Set')?> "> 
+    <?php if($Competitor){ ?>
+        <a href="<?= PageIndex() ?>Actions/Competitor.Logout"><img src='<?= PageIndex()?>Image/Icons/signout.png' width='12px'> <font color="red"><?= ml('Competitor.SignOut')?></font></a>&nbsp;
+    <?php } ?>
     <?php $Language=$_SESSION['language_select']; ?>    
     <?= ImageCountry($Language,20); ?>
     <select style="width:85px;" onchange="form.submit()" name='language'>
