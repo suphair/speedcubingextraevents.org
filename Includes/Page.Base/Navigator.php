4b6193141d74e97ed0686313eb4d188c7d99c6d6
SUPHAIR <suphair@gmail.com>
2020-03-07 09:41:18 +0300

    Edit "Navigator" style

--- Includes/Page.Base/Navigator.php
+++ Includes/Page.Base/Navigator.php
@@ -2,23 +2,53 @@
 if(explode('.',$type)[0]=='aNews')$type='News';
 if(explode('.',$type)[0]=='Delegate' or explode('.',$type)[0]=='Delegates')$type='Delegates';
 if(explode('.',$type)[0]=='Competitor')$type='Competitors';
-if(explode('.',$type)[0]=='Competition')$type='Competitions';
 if(explode('.',$type)[0]=='Event')$type='Events';
+if(explode('.',$type)[0]=='Competition' or $type=='index')$type='Competitions';
 ?>
-<div class="Navigator">
-        <nobr><a <?= ($type=='Competitions' or $type=='index')?"class='select'":"" ?> href="<?= PageIndex() ?>Competitions">
-        <i class="fas fa-cube"></i> <?= ml('Navigator.Competitions') ?></a></nobr>&nbsp;&nbsp;&nbsp;
-        <nobr><a <?= ($type=='Regulations')?"class='select'":"" ?>  href="<?= PageIndex() ?>Regulations">
-        <i class="fas fa-book"></i> <?= ml('Navigator.Regulations') ?></a></nobr>&nbsp;&nbsp;&nbsp;
-        <nobr><a <?= ($type=='Records')?"class='select'":"" ?>  href="<?= PageIndex() ?>Records">
-        <i class="fas fa-trophy"></i> <?= ml('Navigator.Records') ?></a></nobr>&nbsp;&nbsp;&nbsp;
-        <nobr><a <?= ($type=='Competitors')?"class='select'":"" ?>  href="<?= PageIndex() ?>Competitors">
-        <i class="fas fa-users"></i> <?= ml('Navigator.Competitors') ?></a></nobr>&nbsp;&nbsp;&nbsp;
-        <nobr><a <?= ($type=='Events')?"class='select'":"" ?>  href="<?= PageIndex() ?>Events">
-        <i class="fas fa-star"></i> <?= ml('Navigator.Events') ?></a></nobr>&nbsp;&nbsp;&nbsp;
-        <nobr><a <?= ($type=='Delegates')?"class='select'":"" ?>  href="<?= PageIndex() ?>Delegates">
-        <i class="fas fa-sitemap"></i> <?= ml('Navigator.Delegates') ?></a></nobr>&nbsp;&nbsp;&nbsp;
-        <nobr><a <?= ($type=='News')?"class='select'":"" ?>  href="<?= PageIndex() ?>News">
-        <i class="far fa-newspaper"></i> <?= ml('Navigator.News') ?></a></nobr>&nbsp;&nbsp;&nbsp;       
+<div class="navigator">
+        <div data-section="Competitions">
+            <i class="fas fa-cube"></i> <?= ml('Navigator.Competitions') ?>
+        </div>
+    
+        <div data-section="Regulations">
+            <i class="fas fa-book"></i> <?= ml('Navigator.Regulations') ?>
+        </div>
+    
+        <div data-section="Records">
+            <i class="fas fa-trophy"></i> <?= ml('Navigator.Records') ?>
+        </div>
+ 
+        <div data-section="Competitors">
+            <i class="fas fa-users"></i> <?= ml('Navigator.Competitors') ?>
+        </div>
+
+        <div data-section="Events">    
+            <i class="fas fa-star"></i> <?= ml('Navigator.Events') ?>
+        </div>
+            
+        <div data-section="Delegates">
+            <i class="fas fa-sitemap"></i> <?= ml('Navigator.Delegates') ?>
+        </div>
+    
+        <div data-section="News">
+            <i class="far fa-newspaper"></i> <?= ml('Navigator.News') ?>
+        </div>
 </div>
+
+
+<script>
+    $('div.navigator div').on("mouseover",function(){
+        $(this).addClass('navigator_hover');
+    });    
+    
+    $('div.navigator div').on("mouseout",function(){
+        $(this).removeClass('navigator_hover');
+    });    
+    
+    $('div.navigator div').on("click",function(){
+        document.location.href='<?= PageIndex()?>' + $(this).data('section');
+    });    
+    
+    $('div.navigator div[data-section="<?= $type ?>"]').addClass("select");
+</script>
 
