edf092bc63b0870a8a44e9d3fed1b85499dc522d
SUPHAIR <suphair@gmail.com>
2020-05-15 11:30:15 +0300

    Fixed selected event

--- Includes/Regulation/Regulations.js
+++ Includes/Regulation/Regulations.js
@@ -2,5 +2,5 @@ $('[data-regulations-event]').change(function () {
     location.href = $('#variables').data('index') + 'Regulations/' + $(this).val();
 });
 
-var competitionevent = $('[data-competitionevent]').data('competitionevent');
-$('[data-competitionevent-list=' + competitionevent +']').addClass('select');
+var event = $('[data-regulations-event]').data('selected');
+$('[data-regulations-event-list=' + event +']').addClass('select');
