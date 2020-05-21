e52e9805e0533f3c85ef098da61ca9fc60c0ab89
SUPHAIR <suphair@gmail.com>
2020-05-21 08:31:19 +0300

    Added list WCA Delegates

--- Includes/Delegate/Delegates.php
+++ Includes/Delegate/Delegates.php
@@ -47,14 +47,50 @@ $delegates = DataBaseClass::getRowsObject("
             D.Name
 ");
 
+$delegatesWca = DataBaseClass::getRowsObject("
+    SELECT 
+        MAX(Cn.EndDate) endDate,
+        C.WCAID wcaid, 
+        C.Name name, 
+        C.Country country,
+        COUNT( DISTINCT Cm.ID) countCompetitors,
+        COUNT( DISTINCT Cn.ID) countCompetitions
+    FROM CompetitionReport CR
+    JOIN Competitor C ON C.WID=CR.DelegateWCA 
+    JOIN Competition Cn ON Cn.ID=CR.Competition
+        LEFT OUTER JOIN Event E ON E.Competition=Cn.ID
+        LEFT OUTER JOIN Command Com ON 
+            Com.Event=E.ID 
+            AND Com.Decline!=1
+        LEFT OUTER JOIN CommandCompetitor CC ON CC.Command=Com.ID
+        LEFT OUTER JOIN Competitor Cm ON Cm.ID=CC.Competitor 
+        WHERE 1 = 1
+        GROUP BY C.WCAID, C.Name, C.Country
+        ORDER BY 
+            C.Country, 
+            C.Name
+");
+
 foreach ($delegates as &$delegate) {
     $delegate->link = LinkDelegate($delegate->wcaid);
     $delegate->endDate = date_range($delegate->endDate);
     $delegate->country = getObjectCountry($delegate->country);
 }
+unset($delegate);
+
+
+
+
+foreach ($delegatesWca as &$delegate) {
+    $delegate->link = "https://www.worldcubeassociation.org/persons/".$delegate->wcaid;
+    $delegate->endDate = date_range($delegate->endDate);
+    $delegate->country = getObjectCountry($delegate->country);
+}
+unset($delegate);
 
 $data = arrayToObject([
     'delegates' => $delegates,
+    'delegatesWca' => $delegatesWca,
     'candidates' => [
         "show" => CheckAccess('Delegate.Candidates'),
         "link" => LinkDelegate("Candidates")
--- Includes/Delegate/Delegates.t.php
+++ Includes/Delegate/Delegates.t.php
@@ -65,3 +65,56 @@
         <?php } ?>
     </tbody>
 </table>
+
+
+
+<h1><?= ml('Delegates.WCA') ?></h1>
+<table class="table_new">
+    <thead>
+        <tr>
+            <td/>
+            <td>
+                <?= ml('Delegates.Country') ?>
+            </td>
+            <td>
+                <?= ml('Delegates.Name') ?>
+                <i class="fas fa-external-link-alt"></i>
+            </td>
+            <td>
+                <?= ml('Delegates.Competitions') ?>
+            </td>
+            <td>
+                <?= ml('Delegates.Persons') ?>
+            </td>
+            <td>
+                <?= ml('Delegates.LatestActivity') ?>
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($data->delegatesWca as $delegate) { ?>
+            <tr>
+                <td>
+                    <?= $delegate->country->image ?>
+                </td>
+                <td class="table_new_bold">
+                    <?= $delegate->country->name ?>
+                </td>
+                <td> 
+                    <a href="<?= $delegate->link ?>" target="_blank">
+                        <?= $delegate->name ?>
+                    </a>
+                </td>
+                <td class="table_new_right">
+                    <?= $delegate->countCompetitions ?>
+                </td>
+                <td class="table_new_right">
+                    <?= $delegate->countCompetitors ?>
+                </td>
+                <td class="table_new_right">
+                    <?= $delegate->endDate ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
