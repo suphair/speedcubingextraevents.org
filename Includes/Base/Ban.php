64b259e39667a404918b1e0b032e5905f5d2cfdd
SUPHAIR <suphair@gmail.com>
2020-09-18 15:43:33 +0300

    Update Ban.php

--- Includes/Base/Ban.php
+++ Includes/Base/Ban.php
@@ -1,8 +1,51 @@
-<?php 
-$data=ban::get_data();
+<?php
+$data = ban::get_data();
 ban::clear_data();
 ?>
-<h1 class="error">You are banned!</h1>
-<h2><?= $data['competitor']->wca_id ?>: <?= $data['competitor']->name ?></h2>
-<p><?= date_range($data['start_date'],$data['end_date']) ?></p>
-<p><?= $data['reason'] ?></p>
+<br>
+<h2 class="error">
+    <i class="fas fa-user-slash"></i>
+    You are banned!
+</h2>
+<table class="table_info">
+    <tr>
+        <td>
+            Name 
+        </td>
+        <td>
+            <?= $data['competitor']->name ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            WCA ID 
+        </td>
+        <td>
+            <?= $data['competitor']->wca_id ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            From date 
+        </td>
+        <td>
+            <?= date_range($data['start_date']) ?>
+        </td>
+    </tr>   
+    <tr>
+        <td>
+            Until date
+        </td>
+        <td>
+            <?= date_range($data['end_date']) ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Reason
+        </td>
+        <td class="error">
+            <?= $data['reason'] ?>
+        </td>
+    </tr>
+</table>
